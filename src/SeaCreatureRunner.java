f3aef275e89a9a936b353cd8aa92019887a13fe8
TJHL <dont change this>
2017-07-15 11:26:23 -0700

    July 15 2017

---
+++ src/SeaCreatureRunner.java
@@ -0,0 +1,16 @@
+
+public class SeaCreatureRunner {
+public static void main(String[] args) {
+	SeaCreature Spongebob = new SeaCreature("spongebob");
+	Spongebob.eat();
+	Spongebob.laugh();
+	SeaCreature Patrick = new SeaCreature("patrick");
+	Patrick.eat();
+	Patrick.laugh();
+	SeaCreature Squidward = new SeaCreature("squidward");
+	Squidward.eat();
+	Squidward.laugh();
+	
+}
+
+}
