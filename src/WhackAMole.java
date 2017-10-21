e17b3b169411898767325377a98a70fa24e56c17
DavidAndBlake <League@AdministorsiMac.attlocal.net>
2017-10-21 16:01:03 -0700

    Oct, 21

--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -1,27 +1,59 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class WhackAMole {
+
+public class WhackAMole implements ActionListener {
 	public static void main(String[] args) {
-		
+		WhackAMole whack = new WhackAMole();
+		Random r = new Random();
+		int rand = r.nextInt(25); 
+		whack.drawButtons(rand);
+	
 	}
+	int j;
 	JFrame frame;
 	JPanel p;
-	private void drawButtons(){
+	JButton buttons[];
+	int count = 0;
+	private void drawButtons(int rand) {
 		frame = new JFrame();
 		p = new JPanel();
 		frame.add(p);
-		JButton buttons[] = new JButton[24];
+		buttons = new JButton[25];
 		for(int i=0 ; i < buttons.length ; i++){
 			buttons[i]=new JButton();
 			p.add(buttons[i]);
 			buttons[i].setVisible(true);
+			buttons[i].addActionListener(this);
 		}
-		frame.setSize(1000, 1000);
+		buttons[rand].setText("mole!");
+		j = rand;
+		System.out.println(j);
+		frame.setSize(400, 200);
 		frame.setVisible(true);
-		
-		
+	}
+	void speak(String words) {
+		try {
+		Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+		e.printStackTrace();
+		}
+		}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==(buttons[j])) {
+			speak("ow");
+			frame.dispose();
+		 count = count+1;
+		 System.out.println(count);
+		}
 	}
 }
